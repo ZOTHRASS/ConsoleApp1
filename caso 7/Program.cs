f3854f6fb417ec415e260c30b6d56b4a70f1c4a3
tobias <mocciatobias155@gmail.com>
2025-03-19 22:49:06 -0300

    tarea

--- caso 7/Program.cs
+++ caso 7/Program.cs
@@ -1,30 +1,31 @@
-﻿Console.WriteLine("coloca un numero del 1 al 7");
-int numero = int.Parse(Console.ReadLine());
-switch (numero) 
+﻿int numero = int.Parse(Console.ReadLine());
+do
 {
-    case 1:
-        Console.WriteLine("Es Lunes");
-        break;
-    case 2:
-        Console.WriteLine("Es Martes");
-        break;
-    case 3:
-        Console.WriteLine("Es Miercoles");
-        break;
-    case 4:
-        Console.WriteLine("Es Jueves");
-        break;
-    case 5:
-        Console.WriteLine("Es Viernes");
-        break;
-    case 6:
-        Console.WriteLine("Es Sabado");
-        break;
-    case 7:
-        Console.WriteLine("Es Domingo");
-        break;
-}
-while (numero != 8)
- 
+    Console.WriteLine("coloca un numero del 1 al 7");
+    switch (numero)
+    {
+        case 1:
+            Console.WriteLine("Es Lunes");
+            break;
+        case 2:
+            Console.WriteLine("Es Martes");
+            break;
+        case 3:
+            Console.WriteLine("Es Miercoles");
+            break;
+        case 4:
+            Console.WriteLine("Es Jueves");
+            break;
+        case 5:
+            Console.WriteLine("Es Viernes");
+            break;
+        case 6:
+            Console.WriteLine("Es Sabado");
+            break;
+        case 7:
+            Console.WriteLine("Es Domingo");
+            break;
+    }
+} while (numero != 8);
 
 
